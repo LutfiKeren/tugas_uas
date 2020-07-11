627bab4be9ce7486956ef03c73ebc6e59d85e3e4
LutfiKeren <61748642+LutfiKeren@users.noreply.github.com>
2020-07-11 17:18:16 +0700

    Add files via upload

---
+++ pendaftaran-siswa-uas/dts_sertifikat.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 100132
+ Source Host           : localhost:3306
+ Source Schema         : dts_sertifikat
+
+ Target Server Type    : MySQL
+ Target Server Version : 100132
+ File Encoding         : 65001
+
+ Date: 11/07/2020 11:00:16
+*/
+
